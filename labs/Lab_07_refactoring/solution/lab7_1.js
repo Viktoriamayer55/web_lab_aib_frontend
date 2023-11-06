8ea62ba2aeafff5b5383ca10c7d87901a094e51a
Viktoria Panchenko <viktoriabertilova@gmail.com>
2023-11-06 11:52:11 +0300

    do zadanie2

---
+++ labs/Lab_07_refactoring/solution/lab7_1.js
@@ -0,0 +1,13 @@
+//Задание 1.
+let variableOne = prompt('Variable one', '');
+let variableTwo = prompt('Variable two', '');
+
+if (variableOne === variableTwo) {
+    console.log('equally')
+}else{
+    console.log('Not equally');
+} 
+
+let word = 'world'; 
+variableOne += word;
+
