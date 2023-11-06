9ab5e421e34f96c735f634f2baf42a2f8d1a0f6e
Viktoria Panchenko <viktoriabertilova@gmail.com>
2023-11-06 11:42:10 +0300

    Zadanie2

---
+++ labs/Lab_07_refactoring/solution/lab7.js
@@ -0,0 +1,36 @@
+//Задание 1.
+let variableOne = prompt('Variable one', '');
+let variableTwo = prompt('Variable two', '');
+
+if (variableOne === variableTwo) {
+    console.log('equally')
+}else{
+    console.log('Not equally');
+} 
+
+let word = 'world'; 
+variableOne += word;
+
+//Задание 2.
+let arrayFruits = new Array('apple', 'strawberry', 'blueberry', 'raspberry', 'lemon');
+
+for (let i = 0; i < arrayFruits.length; i++) {
+    console.log(arrayFruits[i]);
+    
+    if (arrayFruits[i] === 'apple') {
+        console.log('apple green');
+    }
+    else if (arrayFruits[i] === 'strawberry') {
+        console.log('strawberry red');
+    }
+    else if (arrayFruits[i] === 'blueberry') {
+        console.log('blueberry blue');
+    }
+    else if (arrayFruits[i] === 'raspberry') {
+        console.log('raspberry light red');
+    }
+    else if (arrayFruits[i] === 'lemon') {
+        console.log('lemon yellow');
+    }
+}
+
