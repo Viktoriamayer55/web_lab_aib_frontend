94b22e4a08000af110b6df79dbc593cbb0e058be
Panchenko Viktoria <viktoriabertilova@gmail.com>
2023-12-25 16:07:41 +0300

    addTask2Lab12

---
+++ labs/Lab_12_python_intro/solution/Task2/Task2.py
@@ -0,0 +1,21 @@
+file = open('text2.txt')
+n = int(file.readline())
+Sub = file.readline().split()
+X = list(map(int, Sub))
+medians = []
+
+for i in range (n):
+    sequence = X[:i + 1]
+    sequence.sort()
+
+    if (i + 1) % 2 != 0:
+        medianSeq = sequence [(i + 1) // 2]
+    else:
+        medianSeq = sequence [i // 2]
+    medians.append(medianSeq)
+medianSum = sum(medians)
+
+output = open('output.txt', 'w')
+output.write(str(medianSum))
+
+
