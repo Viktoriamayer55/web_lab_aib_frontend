8b071ad385d8ba310d6eb6ff552d5fa3f6b7acff
Panchenko Viktoria <viktoriabertilova@gmail.com>
2023-12-26 12:24:21 +0300

    task3

---
+++ labs/Lab_12_python_intro/solution/Task3/task3.py
@@ -0,0 +1,30 @@
+def main(line):
+    maxValue = 0
+    myList = {}
+    for item in line:
+        if item == ' ':
+            continue
+        if item in myList:
+            myList[item] += 1
+            maxValue = max(maxValue, myList[item])
+        else:
+            myList[item] = 1
+
+    charValue = list(myList.keys())
+    charValue.sort()
+    i = maxValue
+    while i > 0:
+        str = ""
+        for item in charValue:
+            if myList[item] >= i:
+                str += '#'
+            else:
+                str += ' '
+        print(str)
+        i -= 1
+    for c in charValue:
+        print(c, end='')
+
+if __name__=='__main__':
+    line = input()
+    main(line)
